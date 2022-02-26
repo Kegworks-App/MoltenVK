e7dbf7f276639152909b1c4125a72675f400778a
Dean M Greer <gcenx83@gmail.com>
2022-02-25 20:01:25 -0500

    DXVK Required Changes
    Fake extensions for DXVK;
    - _features.geometryShader
    - _features.tessellationShader
    - _features.shaderCullDistance
    
    Enabled defaults;
    - MVK_CONFIG_FULL_IMAGE_VIEW_SWIZZLE=1
    - MVK_ALLOW_METAL_FENCES=1
    - MVK_CONFIG_RESUME_LOST_DEVICE=1

--- MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
+++ MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
@@ -1716,6 +1716,8 @@ void MVKPhysicalDevice::initFeatures() {
     _features.robustBufferAccess = true;  // XXX Required by Vulkan spec
     _features.fullDrawIndexUint32 = true;
     _features.independentBlend = true;
+    _features.geometryShader = true;  // XXX Required by DXVK for D3D_FEATURE_LEVEL_*_*
+    _features.tessellationShader = true;  // XXX Required by DXVK for D3D_FEATURE_LEVEL_11_0
     _features.sampleRateShading = true;
     _features.depthBiasClamp = true;
     _features.fillModeNonSolid = true;
@@ -1729,6 +1731,7 @@ void MVKPhysicalDevice::initFeatures() {
     _features.shaderUniformBufferArrayDynamicIndexing = true;
     _features.shaderStorageBufferArrayDynamicIndexing = true;
     _features.shaderClipDistance = true;
+    _features.shaderCullDistance = true;  // XXX Required by DXVK for D3D_FEATURE_LEVEL_9_1
     _features.shaderInt16 = true;
     _features.multiDrawIndirect = true;
     _features.inheritedQueries = true;
